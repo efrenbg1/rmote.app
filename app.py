0dc29d6fcc69833c1b544d62ffd5a1bdcce284f5
Efrén Boyarizo <efren@boyarizo.es>
2020-06-29 18:54:20 +0200

    Started with new integration of websockets

--- app.py
+++ app.py
@@ -1,15 +1,16 @@
 from modules import modules, control, manager, settings, register
 from libs import core, ddbb, password, sessions
-from flask import Flask, redirect, render_template
+from libs.flask import app, socketio, template_dir
+from flask import Flask, redirect, render_template, request, send_from_directory
 import sys
 import os
 import checksumdir
 import jsmin
-sys.path.insert(0, './libs')
+from termcolor import colored
+from datetime import datetime
+import werkzeug
+import traceback
 
-app = Flask(__name__, template_folder='static/')
-app.register_blueprint(core.hub)
-app.register_blueprint(modules.hub)
 
 print('Minifying JS...')
 tempMin = ""
@@ -28,6 +29,13 @@ hash = checksumdir.dirhash(os.path.join(os.getcwd(), 'static'))[0:4]
 print("Hash: " + hash)
 
 
+@app.before_request
+def before_request_func():
+    now = datetime.now()
+    log = colored(now.strftime("%H:%M:%S"), 'blue') + " → " + request.path
+    print(log)
+
+
 @app.route('/')
 def index():
     return render_template('index.html', hash=hash)
@@ -40,9 +48,28 @@ def redirectNoFile():
 
 @app.route('/<path:path>')
 def static_files(path):
-    return app.send_static_file(path)
+    return send_from_directory(template_dir, path)
+
+
+@app.errorhandler(404)
+def not_found(e):
+    return render_template('index.html', hash=hash)
+
+
+@app.errorhandler(werkzeug.exceptions.MethodNotAllowed)
+def handle_bad_request(e):
+    return """<img src="https://meme.xyz/uploads/posts/t/45972-you-have-no-power-here.jpg">"""
+
+
+@socketio.on_error()
+@app.errorhandler(Exception)
+def catch(e):
+    print(colored("\n[ERROR]", "red", attrs=["bold"]))
+    print(traceback.print_exc())
+    print("")
+    return "400 (Bad request)", 400
 
 
 if __name__ == '__main__':
     app.config['TEMPLATES_AUTO_RELOAD'] = True
-    app.run(host='0.0.0.0', port=80, threaded=True)
+    socketio.run(app, host='0.0.0.0', port=80)
---
+++ debug.py
@@ -0,0 +1,45 @@
+import subprocess
+import threading
+from termcolor import colored
+import time
+import os
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+from livereload import Server
+
+command = ["python", "app.py"]
+
+app = subprocess.Popen(command)
+
+
+class changesListener(FileSystemEventHandler):
+    def __init__(self, app):
+        self.t = 0.0
+        self.mutex = threading.Lock()
+        self.app = app
+
+    def on_any_event(self, event):
+        if event.src_path.endswith(".min.js"):
+            return
+        py = event.src_path.endswith(".py")
+        js = event.src_path.endswith(".js")
+        if (py or js) and (time.time() - self.t) > 5 and not self.mutex.locked():
+            self.mutex.acquire()
+            print(colored("\nReloading app...\n\n", "red"))
+            self.t = time.time()
+            time.sleep(1)
+            self.app.kill()
+            self.app = subprocess.Popen(command)
+            self.mutex.release()
+
+
+event_handler = changesListener(app)
+observer = Observer()
+observer.schedule(event_handler, path=os.getcwd(), recursive=True)
+observer.start()
+
+server = Server()
+server.watch('static/sections.min.js', delay=2)
+server.watch('static/index.min.css', delay=2)
+server.watch('static/index.html', delay=2)
+server.serve(liveport=35729, debug=False)
